74a2527b4842f5fa1c5c331878096a301fb88cb4
Oleksandr Shyhyda <BayanAlex@gmail.com>
2023-05-06 16:55:23 +0300

    feat: moved db connection string to .env

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
-
+require('dotenv').config();
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(process.env.MONGO_URL || '');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
